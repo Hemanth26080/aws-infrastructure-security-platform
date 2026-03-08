110cee6ffa2669098dbc9d7403cc8277181d3f2d
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-03-08 21:09:28 +0100

    dev-infra

---
+++ 40-databases/variables.tf
@@ -0,0 +1,34 @@
+variable "project_name" {
+  default = "roboshop"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "sg_names" {
+    default = [
+        # databases
+        "mongodb", "redis", "mysql", "rabbitmq",
+        # backend
+        "catalogue", "user", "cart", "shipping", "payment",
+        # frontend
+        "frontend",
+        # bastion
+        "bastion",
+        # frontend load balancer
+        "frontend_alb",
+        # Backend ALB
+        "backend_alb",
+        "open_vpn"
+    ]
+}
+
+variable "zone_id" {
+    default = "Z09571432H0XSZWLPQGY5"
+}
+
+variable "domain_name" {
+    default = "phemanth.in"
+}
+
