cbe016c775fccfcbfec9371d27427f4acc1197db
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-03-08 20:51:08 +0100

    dev-infra

---
+++ 21-vpn/variables.tf
@@ -0,0 +1,17 @@
+variable "project_name" {
+    type = string
+    default = "roboshop"
+}
+
+variable "environment" {
+    type = string
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z09571432H0XSZWLPQGY5"
+}
+
+variable "domain_name" {
+    default = "phemanth.in"
+}
