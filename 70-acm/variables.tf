f08ba6b53ec1fd54da38777e37e607c25c577cd5
Hemanth Ponugothi <phemanth8888@gmail.com>
2026-03-09 23:33:10 +0100

    infra-dev

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z09571432H0XSZWLPQGY5"
+}
+
+variable "domain_name" {
+    default = "phemanth.in"
+}
